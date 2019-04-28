056327244efe7877b229af7b64bcfb0a52d27ebf
louie <l.e.ormston@gmail.com>
2019-04-28 20:35:31 +0100

    Homepage voting system complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @resources1 = Resource.order('hard DESC').limit(5)
+    @resources2 = Resource.order('easy DESC').limit(5)
+    @resources3 = Resource.order('useful DESC').limit(5)
   end
 end
